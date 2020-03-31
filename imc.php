05f38e7005872d164fd161e5bc2aa28baece6358
Anaglebia <bia22.sp@icloud.com>
2020-03-31 20:15:26 -0300

    aula1

---
+++ imc.php
@@ -0,0 +1,19 @@
+<?php
+// clase imc
+class indicemassacarporal {
+    public $pesoProp;
+    Public $alturaProp;
+
+    public function calculaMassaCorporal(){
+        $imc = ($this ->pesoProp / ($this ->alturaProp * $this ->alturaprop));
+        return $imc;
+    }
+    public fucntion situacaoPeso ();
+    $x = $this -> calculaMassaCorporal();
+
+    if($x = <20 )
+    
+    imc < 20 = abaixo do peso
+    imc >= 20 && imc <25 =peso ideal 
+    imc > 25
+}
---
+++ retangulo.php
@@ -0,0 +1,13 @@
+<?php
+//classe retangulo
+class reteangulo{
+    public $basePropep;
+    public $alturaProp;
+
+    //metodos
+    
+    public fuction calcularArea(){
+        $area = ($this -> baseProp * $this -> $alturaPropep);
+        return $area;
+    }
+}
